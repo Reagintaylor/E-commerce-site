71c5062cb93563f88c1979cadfa3d3cdae3da5d2
Reagin Turner <rturnerafteragnes@gmail.com>
2021-11-23 14:18:23 -0500

    Adding the models and associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,10 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Products, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tags, {
+  foreignKey: '',
+  
+})
 
 // Tags belongToMany Products (through ProductTag)
 
